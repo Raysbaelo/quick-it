0af119e3902e1583814f4a74518e21f047308d6a
Zuzana Chudinova <zuzka.chudinova@gmail.com>
2021-12-10 15:17:31 +0100

    js slider strat

--- script.js
+++ script.js
@@ -8,3 +8,30 @@ function navToggle() {
 }
 
 btn.addEventListener("click", navToggle);
+
+const slider = document.querySelector(".slider-container"),
+  slides = Array.from(document.querySelectorAll(".slide"));
+
+let isDragging = false,
+  startPos = 0,
+  currentTranslate = 0,
+  prevTranslate = 0,
+  animationID = 0,
+  currentIndex = 0;
+
+slides.forEach((slide, index) => {
+  constslideImage = slide.querySelector("img");
+  slideImage.addEventListener("dragstart", (e) => e.preventDefault());
+  // touch events
+  slide.addEventListener("touchstart", touchStart(index));
+  slide.addEventListener("touchend", touchEnd);
+  slide.addEventListener("touchmove", touchMove);
+
+  // mouse events
+  slide.addEventListener("mousedown", touchStart(index));
+  slide.addEventListener("mouseup", touchEnd);
+  slide.addEventListener("mouseleave", touchEnd);
+  slide.addEventListener("mousemove", touchMove);
+});
+
+function touchStart(index)
