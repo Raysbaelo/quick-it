4b80a7e274f5ef487b3ba74ba6f4f4e6358200f1
Agata Szulc <agataszulc97@gmail.com>
2021-12-15 18:39:14 +0100

    displaying data from database, no styling

--- restaurants.js
+++ restaurants.js
@@ -6,4 +6,16 @@ fetch(url)
 
 function showRestaurant(restaurant) {
   console.log(restaurant);
+  const template = document.querySelector("template").content;
+  const clone = template.cloneNode(true);
+  clone.querySelector("a").href = "agata.dk";
+  clone.querySelector(".restaurantBackground").src =
+    restaurant._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url;
+  clone.querySelector(".restaurantLogo").src =
+    restaurant._embedded["wp:featuredmedia"][0].media_details.sizes.thumbnail.source_url;
+  clone.querySelector("h3").textContent = restaurant.title.rendered;
+  const parent = document.querySelector(".restaurantGrid");
+  parent.appendChild(clone);
 }
+
+//what about alt???
