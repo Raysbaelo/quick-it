170b57688ef2e4208f5a0efa51f5c9eac80d1070
Agata Szulc <agataszulc97@gmail.com>
2021-12-15 23:04:59 +0100

    minor styling, not ready yet

--- restaurants.js
+++ restaurants.js
@@ -27,12 +27,12 @@ function showRestaurant(restaurant) {
   const template = document.querySelector("template").content;
   const clone = template.cloneNode(true);
 
-  clone.querySelector("a").href = "agata.dk";
+  clone.querySelector("a").href = restaurant.website;
   clone.querySelector(".restaurantBackground").src =
     restaurant._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url;
   clone.querySelector(".restaurantLogo").src = `http://agata.dk/qui/${restaurant.id}.png`;
   console.log(`http://agata.dk/qui/${restaurant.id}.png`);
-  clone.querySelector("h3").textContent = restaurant.title.rendered;
+  clone.querySelector(".restaurantName").textContent = restaurant.title.rendered;
 
   const parent = document.querySelector(".restaurantGrid");
   parent.appendChild(clone);
