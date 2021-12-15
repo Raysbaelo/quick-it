3e23d4731cce432565a639d80a9b963118abf41c
Agata Szulc <agataszulc97@gmail.com>
2021-12-15 19:31:54 +0100

    some styling done to fetched elements

--- restaurants.js
+++ restaurants.js
@@ -5,17 +5,17 @@ fetch(url)
   .then((data) => data.forEach(showRestaurant));
 
 function showRestaurant(restaurant) {
-  console.log(restaurant);
+  console.log(restaurant.id);
   const template = document.querySelector("template").content;
   const clone = template.cloneNode(true);
+
   clone.querySelector("a").href = "agata.dk";
   clone.querySelector(".restaurantBackground").src =
     restaurant._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url;
-  clone.querySelector(".restaurantLogo").src =
-    restaurant._embedded["wp:featuredmedia"][0].media_details.sizes.thumbnail.source_url;
+  clone.querySelector(".restaurantLogo").src = `http://agata.dk/qui/${restaurant.id}.png`;
+  console.log(`http://agata.dk/qui/${restaurant.id}.png`);
   clone.querySelector("h3").textContent = restaurant.title.rendered;
+
   const parent = document.querySelector(".restaurantGrid");
   parent.appendChild(clone);
 }
-
-//what about alt???
