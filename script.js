966808dfcd6a01c0cd98670c33b6c0b11dad76ab
Zuzana Chudinova <zuzka.chudinova@gmail.com>
2021-12-19 22:10:47 +0100

    again forgot

--- script.js
+++ script.js
@@ -44,3 +44,23 @@ nextBtn.addEventListener("click", () => {
   slides[slideNumber].classList.add("active");
   slideIcons[slideNumber].classList.add("active");
 });
+
+// image slider previous button  
+prevBtn.addEventListener('click', () => {
+  slides.forEach((slide) => {
+    slide.classList.remove('active');
+  });
+  slideIcons.forEach((slideIcon) => {
+    slideIcon.classList.remove('active');
+  });
+
+  slideNumber--;
+
+  if (slideNumber < 0) {
+    slideNumber = numberOfSlides - 1;
+  }
+
+  slides[slideNumber].classList.add('active');
+  slideIcons[slideNumber].classList.add('active');
+
+});
